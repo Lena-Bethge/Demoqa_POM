d40d1a4d0c1bfe9939745861d0daac424349ef8d
Lena Natykan Bethge <elena.natykan@gmail.com>
2022-10-27 00:05:13 +0200

    Add student data

---
+++ src/main/java/com/telran/data/StudentData.java
@@ -0,0 +1,17 @@
+package com.telran.data;
+
+public class StudentData {
+
+    public static final String FIRST_NAME = "Jack";
+    public static final String LAST_NAME = "Joy";
+    public static final String EMAIL = "joy@gmail.com";
+    public static final String GENDER = "Male";
+    public static final String TEL_NUM = "9876543210";
+    public static final String DATE = "1 May 1987";
+    public static final String[] SUBJECTS = {"Maths","Physics","English"};
+    public static final String[] HOBBIES = {"Sports","Reading"};
+    public static final String PHOTO_PATH = "C:/Tools1/1987.jpg";
+    public static final String ADDRESS= "Leipzig";
+    public static final String STATE = "NCR";
+    public static final String CITY = "Delhi";
+}
--- src/main/java/com/telran/data/UserData.java
+++ src/main/java/com/telran/data/UserData.java
@@ -4,4 +4,6 @@ public class UserData {
 
     public static final String USER_NAME = "Lee";
     public static final String USER_PASSWORD = "1234Qwer!";
+    public static final String USER_EMAIL = "lee@gmail.com";
+
 }
