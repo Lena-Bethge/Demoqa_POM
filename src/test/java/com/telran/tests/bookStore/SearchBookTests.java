69dbabc3db2615ecec031f781cbf4f49c5b5309b
Lena Natykan Bethge <elena.natykan@gmail.com>
2022-11-02 00:30:25 +0100

    Add tests

--- src/test/java/com/telran/tests/bookStore/SearchBookTests.java
+++ src/test/java/com/telran/tests/bookStore/SearchBookTests.java
@@ -6,6 +6,7 @@ import com.telran.pages.HomePage;
 import com.telran.tests.TestBase;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class SearchBookTests extends TestBase {
@@ -19,5 +20,13 @@ public class SearchBookTests extends TestBase {
     public void searchBookPositiveTest() {
         new BookStorePage(driver).typeInSearchBookInput(BookData.BOOK_NAME);
         Assert.assertTrue(new BookStorePage(driver).takeNameOfBook().contains(BookData.BOOK_NAME));
+    }
+
+
+     @Test
+     @Parameters({"BOOK_NAME"})
+    public void testngSearchBookPositiveTest(String BOOK_NAME) {
+        new BookStorePage(driver).typeInSearchBookInput(BOOK_NAME);
+        //Assert.assertTrue(new BookStorePage(driver).takeNameOfBook().contains(BOOK_NAME));
     }
 }
